92097e08202fbbd2a470c4b89caac4282c67b26f
Lebogang <>
2023-11-29 12:26:04 +0200

    second

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from "./components/login"
+
+import Signup from './components/signup';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login/>
+      <Signup/>
       <StatusBar style="auto" />
     </View>
   );
