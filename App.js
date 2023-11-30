db3e2a02d7f188209d315983a395b1bf69e5f7b4
Lebogang <>
2023-11-30 14:10:25 +0200

    fourth

--- App.js
+++ App.js
@@ -1,45 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Chathistory from './components/Chathistory';
-=======
-import Edit from './components/Edit';
-import Profile from './components/Profile';
->>>>>>> profile
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-<<<<<<< HEAD
-      <Chathistory/>
-=======
-import Login from "./components/login"
-import Forgot from './components/Forgot';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-<<<<<<< HEAD
-      <Login/>
->>>>>>> login
-=======
-
 import Signup from './components/signup';
+import Forgot from './components/Forgot';
+import Login from './components/login';
+import Profile from './components/Profile';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Signup/>
->>>>>>> signup
-=======
-      <Forgot/>
->>>>>>> forgotpassword
-      <StatusBar style="auto" />
-=======
-      <Edit/>
+      <Profile/>
       
->>>>>>> profile
+
     </View>
   );
 }
