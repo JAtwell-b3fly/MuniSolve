d74bb24dfb978e4d45cfdc9a681127fac581204d
Neoseekoei <seekoeineo16@gmail.com>
2023-12-01 09:23:26 +0200

    Boarding

--- App.js
+++ App.js
@@ -1,48 +1,36 @@
-
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-import Chathistory from './components/Chathistory';
-
-import Edit from './components/Edit';
-import Profile from './components/Profile';
-
-=======
->>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import HomePage from "./components/HomePage";
 import Screen from "./components/screens/Screen";
 import ChatHistoryPage from "./components/ChatHistoryPage";
 import ProfilePage from "./components/ProfilePage";
+import Chathistory from './components/Chathistory';
+
+import Edit from './components/Edit';
+import Profile from './components/Profile';
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-<<<<<<< HEAD
-
-=======
->>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
 
 export default function App() {
   const Drawer = createDrawerNavigator();
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
  return(
-=======
-  return (
+// =======
+//   return (
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
-=======
+// =======
    
 
   
->>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
->>>>>>> cddbb05d230711aeb718912b4cd76d105e986da7
+// >>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
+// >>>>>>> cddbb05d230711aeb718912b4cd76d105e986da7
     <NavigationContainer>
       <Drawer.Navigator
         drawerContent={(props) => <ProfilePage {...props} />}
@@ -130,25 +118,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-<<<<<<< HEAD
+// <<<<<<< HEAD
     justifyContent: 'center',
     // height:5,
     // width:50,
     // backgroundColor:'blue',
     // borderRadius:15,
   }
-=======
-<<<<<<< HEAD
+// =======
+// <<<<<<< HEAD
 
-    justifyContent: 'start',
-  },
+//     justifyContent: 'start',
+//   },
 
 
 
-=======
-    justifyContent: 'start',
-  },
+// =======
+//     justifyContent: 'start',
+//   },
 
->>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
->>>>>>> cddbb05d230711aeb718912b4cd76d105e986da7
+// >>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
+// >>>>>>> cddbb05d230711aeb718912b4cd76d105e986da7
 });
