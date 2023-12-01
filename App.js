44b010f3e7fd76c3251c92edc7decce90c353647
Neoseekoei <seekoeineo16@gmail.com>
2023-12-01 12:51:07 +0200

    All Three Boarding Pages

--- App.js
+++ App.js
@@ -14,6 +14,9 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import BoardingPageOne from "./Boarding/BoardingPageOne";
+import BoardingPageTwo from "./Boarding/BoardingPageTwo";
+import BoardingPageThree from "./Boarding/BoardingPageThree";
 
 export default function App() {
   const Drawer = createDrawerNavigator();
@@ -26,82 +29,82 @@ export default function App() {
 // <<<<<<< HEAD
 
 // =======
-   
+   <BoardingPageTwo/>
 
   
 // >>>>>>> a345281c424383c3c05f46bcc3329badee0509ab
 // >>>>>>> cddbb05d230711aeb718912b4cd76d105e986da7
-    <NavigationContainer>
-      <Drawer.Navigator
-        drawerContent={(props) => <ProfilePage {...props} />}
-        initialRouteName="Home"
-        screenOptions={{
-          drawerStyle: {
-            // Add your styles for the drawer here
-            backgroundColor: "#F5F5F5",
-            width: 240, // Set the width of the drawer
-          },
-          drawerActiveTintColor: "#2e64e5", // Color of the active item in the drawer
-          drawerInactiveTintColor: "#828282", // Color of the inactive items in the drawer
-          drawerLabelStyle: {
-            // Style for the label text in the drawer
-            fontSize: 16,
-          },
-          drawerItemStyle: {
-            // Style for each drawer item
-            marginBottom: 10,
-          },
-          drawerContentContainerStyle: {
-            // Style for the container of the drawer content
-            paddingVertical: 20,
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="Home"
-          component={HomePage}
-          options={{
-            drawerIcon: ({ focused, size }) => (
-              // You can use any icon library or your custom component here
-              // Example with a simple icon using the Material Icons library
-              <AntDesign name="home" size={24} color="gray" />
-            ),
-          }}
-        />
+//     <NavigationContainer>
+//       <Drawer.Navigator
+//         drawerContent={(props) => <ProfilePage {...props} />}
+//         initialRouteName="Home"
+//         screenOptions={{
+//           drawerStyle: {
+//             // Add your styles for the drawer here
+//             backgroundColor: "#F5F5F5",
+//             width: 240, // Set the width of the drawer
+//           },
+//           drawerActiveTintColor: "#2e64e5", // Color of the active item in the drawer
+//           drawerInactiveTintColor: "#828282", // Color of the inactive items in the drawer
+//           drawerLabelStyle: {
+//             // Style for the label text in the drawer
+//             fontSize: 16,
+//           },
+//           drawerItemStyle: {
+//             // Style for each drawer item
+//             marginBottom: 10,
+//           },
+//           drawerContentContainerStyle: {
+//             // Style for the container of the drawer content
+//             paddingVertical: 20,
+//           },
+//         }}
+//       >
+//         <Drawer.Screen
+//           name="Home"
+//           component={HomePage}
+//           options={{
+//             drawerIcon: ({ focused, size }) => (
+//               // You can use any icon library or your custom component here
+//               // Example with a simple icon using the Material Icons library
+//               <AntDesign name="home" size={24} color="gray" />
+//             ),
+//           }}
+//         />
         
-        <Drawer.Screen 
-        name="Chat History" 
-        component={ChatHistoryPage}
-        options={{
-          drawerIcon: ({ focused, size}) => (
-            <FontAwesome name="history" size={24} color="gray" />
-          ),
-        }} />
-        <Drawer.Screen 
-        name="My Profile" 
-        component={ProfilePage} 
-        options={{
-          drawerIcon: ({ focused, size}) => (
-            <FontAwesome name="user-circle-o" size={24} color="gray" />
-          ),
-        }}
-        />
+//         <Drawer.Screen 
+//         name="Chat History" 
+//         component={ChatHistoryPage}
+//         options={{
+//           drawerIcon: ({ focused, size}) => (
+//             <FontAwesome name="history" size={24} color="gray" />
+//           ),
+//         }} />
+//         <Drawer.Screen 
+//         name="My Profile" 
+//         component={ProfilePage} 
+//         options={{
+//           drawerIcon: ({ focused, size}) => (
+//             <FontAwesome name="user-circle-o" size={24} color="gray" />
+//           ),
+//         }}
+//         />
 
-<Drawer.Screen name="Signout" component={props => (
-          <TouchableOpacity style={styles.signOutButton}>
-            <Text>Sign Out</Text>
-          </TouchableOpacity>
-        )}
-        options={{
-          drawerIcon: ({ focused, size}) => (
-            <MaterialIcons name="logout" size={24} color="gray" />
-          ),
-        }}
-        />
+// <Drawer.Screen name="Signout" component={props => (
+//           <TouchableOpacity style={styles.signOutButton}>
+//             <Text>Sign Out</Text>
+//           </TouchableOpacity>
+//         )}
+//         options={{
+//           drawerIcon: ({ focused, size}) => (
+//             <MaterialIcons name="logout" size={24} color="gray" />
+//           ),
+//         }}
+//         />
         
-      </Drawer.Navigator>
+//       </Drawer.Navigator>
       
-    </NavigationContainer>
+//     </NavigationContainer>
 
   );
 }
